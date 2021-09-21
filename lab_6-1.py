bdf733e8bf3443b2772f383be949411d32f00337
p22srogers <90051572+p22srogers@users.noreply.github.com>
2021-09-21 10:07:41 -0400

    Add files via upload

---
+++ lab_6-1.py
@@ -0,0 +1,8 @@
+# Author: SMR (AMDG) 09/21/21
+
+first_name=input("What is your first name? ")
+last_name=input("What is your last name? ")
+full_name=first_name+ " " +last_name
+print(full_name)
+
+
